c55bfa444700505b360700ff4111be8fbc36b297
NaveenMathialagan <mnaveensmn@gmail.com>
2020-05-24 14:16:30 +0530

    Completed the backend for feature 1.

--- onlinebookstore/src/main/java/com/smntech/onlinebookstore/OnlineBookStoreApplication.java
+++ onlinebookstore/src/main/java/com/smntech/onlinebookstore/OnlineBookStoreApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineBookStoreApplication.class, args);
+		SpringApplication.run(OnlineBookStoreApplication.class, args);	
 	}
-
+	
 }
---
+++ onlinebookstore/src/main/java/com/smntech/onlinebookstore/config/RepositoryConfig.java
@@ -0,0 +1,34 @@
+package com.smntech.onlinebookstore.config;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.smntech.onlinebookstore.entity.Book;
+import com.smntech.onlinebookstore.entity.BookCategory;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+	
+	@Autowired
+	private EntityManager entityManager;
+
+	/**
+	 * Used for exposing the id's in Entity class
+	 */
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Book.class);
+		config.exposeIdsFor(BookCategory.class);
+		
+		/*config.exposeIdsFor(entityManager.getMetamodel().getEntities().stream()
+				.map(Type::getJavaType)
+				.toArray(Class[]::new));*/
+		
+		
+	}
+	
+}
