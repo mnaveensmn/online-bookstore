65ff2c73c8dccd0359065b4b73a8618dbae41022
NaveenMathialagan <mnaveensmn@gmail.com>
2020-05-25 14:30:16 +0530

    Created the angular project.

---
+++ angular-bookstore/src/app/common/book.ts
@@ -0,0 +1,15 @@
+import { Identifiers } from '@angular/compiler';
+
+export class Book {
+
+    sku: string;
+    name: string;
+    description: string;
+    unitPrice: number;
+    imageUrl: string;
+    active: boolean;
+    unitsInStock: number;
+    createdOn: Date;
+    updatedOn: Date;
+
+}
